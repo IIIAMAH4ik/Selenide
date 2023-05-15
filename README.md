6d38c198e27162daed97e88ea75473f5d07d6827
IIIAMAH4ik <tembatokmakov43@gmail.com>
2023-05-15 18:58:38 +0300

    Com

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/3u7udg8oc08j14v9/branch/main?svg=true)](https://ci.appveyor.com/project/IIIAMAH4ik/Selenide/branch/main)
---
+++ src/test/java/ru/netology/web/CallbackTest.java
@@ -0,0 +1,20 @@
+package ru.netology.web;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+class CallbackTest {
+
+    @Test
+    void shouldTestForm() {
+        open("http://localhost:9999/");
+        $("[data-test-id=name] input").setValue("Хосе-Игнасио");
+        $("[data-test-id=phone] input").setValue("+79033003030");
+        $("[data-test-id=agreement]").click();
+        $("button").click();
+        $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+}
+
